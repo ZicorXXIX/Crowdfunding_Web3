a6b31d42bd54531585604901a86ea7a534bfaa07
ZicorXXIX <106899726+ZicorXXIX@users.noreply.github.com>
2024-01-26 17:31:02 +0000

    Updated sidebar

--- CrowdFunding/src/components/Sidebar.jsx
+++ CrowdFunding/src/components/Sidebar.jsx
@@ -1,14 +1,28 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useState } from 'react'
 import styles from './Sidebar.module.css'
-import { Icon } from './Icon'
+import { navlinks } from '../constants'
 
 
 
 
 export const Sidebar = () => {
+  const [isActive, setIsActive] = useState("dashboard");
+
+  function handleClick(name){
+    console.log(name);
+    setIsActive(name);
+  }
   return (
+
     <div className={styles.mainDiv}>
-        {/* <Icon /> */}
+      {
+        navlinks.map(({imgUrl, name})=>(
+          <div className={styles.iconDiv} style={{ "backgroundColor" : isActive && isActive === name && "#2c2f32"}}  onClick={()=>(handleClick(name))}>
+            <img key={name} className={styles.icon} style={isActive !== name ? { filter: "grayscale(1)" } : {}} src={imgUrl} alt={name}></img>
+          </div>
+        ))
+      }
+        
     </div>
   )
 }
